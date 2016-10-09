128c6a60991174e098ef27f963fc9b8a64253db5
gorbach-d-v <gorbach.d.v@yandex.ru>
2016-10-09 15:27:19 +0300

    Plus time
    Adding time to collection

--- src/main/java/ru/sandbox/gb/Message.java
+++ src/main/java/ru/sandbox/gb/Message.java
@@ -1,35 +1,54 @@
 package ru.sandbox.gb;
 
 import javax.servlet.http.HttpServletRequest;
-
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.*;
 /**
- * Created by dmitry on 02.10.16.
+ * Created by daniil on 07.10.16.
  */
 public class Message {
 
     private String user;
+    private String time;
     private String body;
 
-    public Message(String user, String body) {
+
+    public Message(String user, String time, String body) {
         this.user = user;
+        this.time = time;
         this.body = body;
     }
 
-    public String getUser() {
-        return user;
-    }
+    public String getUser() { return user; }
+    public String getTime() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss");
+        LocalDateTime TimeNow = LocalDateTime.now();
+        LocalDateTime TimeThen =LocalDateTime.parse(time, formatter);
 
+        if ((ChronoUnit.HOURS.between(TimeThen, TimeNow)<1)) {
+            return (ChronoUnit.MINUTES.between(TimeThen, TimeNow) + " minutes ago");
+        }else if (ChronoUnit.DAYS.between(TimeThen, TimeNow)<1){
+            return (ChronoUnit.HOURS.between(TimeThen, TimeNow)+" hours ago");
+        }else {
+            return TimeThen.format(DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss"));
+        }
+    }
     public String getBody() {
         return body;
     }
 
     public static Message fromRequest(HttpServletRequest req) {
+        LocalDateTime dateTime = LocalDateTime.now();
 
         String user = req.getParameter("user");
         String body = req.getParameter("body");
+        String time = dateTime.format(DateTimeFormatter.ofPattern("d::MMM::uuuu HH::mm::ss"));
 
-        if(user != null && !user.isEmpty() && body != null && !body.isEmpty()) {
-            return new Message(user, body);
+        if(user != null && !user.isEmpty() && body != null && !body.isEmpty()&& time != null && !time.isEmpty()) {
+            return new Message(user, time, body);
         }
 
         return null;
